1fafb95ee1ac56c295d31186dbd1188ab764fdfe
namrouche993 <namrouche993@gmail.com>
2023-08-03 11:53:25 +0100

    adding more example in app7

--- src/App7_useref/App7_concrete_example.js
+++ src/App7_useref/App7_concrete_example.js
@@ -1,7 +1,38 @@
-import React from 'react'
-
+import React, { useRef, useState,useEffect } from 'react'
+let variable = 0;
 export default function App7_concrete_example() {
+  const [sta,setSta] = useState(0);
+  const refin = useRef(0);
+
+  const incrementState = () => {
+    setSta(sta+1)
+    console.log('inside incrementState')
+  }
+
+  const incrementRef = () => {
+    refin.current=refin.current+1;
+    console.log('inside incrementRef')
+
+  }
+
+  const incrementVar = () => {
+    variable = variable+1
+    console.log('inside incrementVar')
+  }
+
+  useEffect(() => {
+    console.log('inside use Effect')
+  })
+  
+
   return (
-    <div>App7_concrete_example</div>
+    <>
+    <button onClick={incrementState}>Incremante State</button>
+    <button onClick={incrementRef}>Incremante Ref</button>
+    <button onClick={incrementVar}>Incremante Variable</button>
+    <br></br>
+    <br></br>
+    State : {sta} --------  Ref : {refin.current} ------- Variable : {variable}
+    </>
   )
 }
